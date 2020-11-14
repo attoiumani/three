148f9e085e53569468f47e713a69051dcdeec04e
shirakawa <attoiumani0313@gmail.com>
2020-11-14 21:03:08 +0900

    Create pixi.vue

---
+++ pages/pixi.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <pixi />
+  </div>
+</template>
+
+<script>
+import pixi from "@/components/pixi.vue";
+
+export default {
+  name: "app",
+  components: {
+    pixi,
+  },
+};
+</script>
