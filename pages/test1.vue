f63c6055edd39184d91020954550b8b706d3c29a
shirakawa <attoiumani0313@gmail.com>
2020-11-14 10:44:56 +0900

    Update test1.vue

--- pages/test1.vue
+++ pages/test1.vue
@@ -1,88 +1,126 @@
-<template lang="html">
-  <div :class="'img-'+this.mainImgID" id="cvs_thumnails_box">
-    <canvas id="cvs">
-    </canvas>
-    <div id="thumbnails">
-      <div v-for="t in thumbnails" @click="changeImg(t.id)" :class="'thumbnail-'+ t.id" class="thumbnail">
-      </div>
-    </div>
-  </div>
+<template>
+  <div id="app"></div>
 </template>
 
 <script>
-import {Container} from 'pixi.js'
-import {WebGLRenderer} from 'pixi.js'
-import {Sprite} from 'pixi.js'
-import {Graphics} from 'pixi.js'
-import {Filter} from 'pixi.js'
-import {Matrix} from 'pixi.js'
+import { PIXI } from 'expo-pixi';
+import { TweenLite } from "gsap";
+
+const SIZE = 512;
+
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.LINEAR;
+PIXI.settings.PRECISION_FRAGMENT = PIXI.PRECISION.HIGH;
+const DPR = window.devicePixelRatio && window.devicePixelRatio >= 2 ? 2 : 1;
+
 export default {
-  props: {
-    'productId': Number
-  },
-  data: () => {
-    return {
-      mainImgID: 0,
-      thumbnails: Array.apply(null, { length: 5 })
-        .map(function (_, idx) {
-          return {
-            id: idx,
+  name: "App",
+  mounted() {
+    this.$options.slides = [];
+
+    this.$options.app = new PIXI.Application({
+      width: SIZE,
+      height: SIZE,
+      antialias: true,
+      transparent: true,
+      resolution: DPR,
+      autoResize: true
+    });
+
+    const clipRect = new PIXI.Rectangle(
+      0,
+      0,
+      this.$options.app.screen.width,
+      this.$options.app.screen.height
+    );
+
+    this.$el.appendChild(this.$options.app.view);
+
+    this.$options.stage = new PIXI.Container();
+    this.$options.stage.filterArea = clipRect;
+
+    this.$options.app.stage.addChild(this.$options.stage);
+
+    const IMAGES = [
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/j110.jpg",
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/j104.jpg"
+    ];
+
+    this.$options.dispSprite = PIXI.Sprite.fromImage(
+      "https://raw.githubusercontent.com/codrops/LiquidDistortion/master/img/dmaps/2048x2048/ripple_2.jpg"
+    );
+
+    this.$options.dispSprite.texture.baseTexture.wrapMode =
+      PIXI.WRAP_MODES.REPEAT;
+    this.$options.dispSprite.skew.x = 0;
+    this.$options.dispSprite.skew.y = 0;
+    this.$options.dispSprite.position.y = 0;
+    this.$options.dispSprite.width = SIZE;
+    this.$options.dispSprite.height = SIZE;
+
+    this.$options.stage.addChild(this.$options.dispSprite);
+
+    this.$options.dispFilter = new PIXI.filters.DisplacementFilter(
+      this.$options.dispSprite,
+      0
+    );
+
+    this.$options.dispFilter.scale.x = 15;
+    this.$options.dispFilter.scale.y = 15;
+
+    this.$options.stage.filters = [this.$options.dispFilter];
+
+    try {
+      PIXI.loader.add(IMAGES).load((l, images) => {
+        Object.keys(images).forEach(key => {
+          let sprite = new PIXI.Sprite(PIXI.loader.resources[key].texture);
+
+          this.$options.slides.push(sprite);
+
+          const SCALE = { x: SIZE / sprite.width, y: SIZE / sprite.height };
+          sprite.scale.set(SCALE.x, SCALE.y);
+          sprite.alpha = 0;
+          this.$options.stage.addChild(sprite);
+        });
+
+        TweenLite.to(this.$options.slides[0], 0.575, {
+          alpha: 1,
+          onCompleted: () => {
+            TweenLite.to(this.$options.slides[0], 0.575, {
+              alpha: 0,
+              delay: 2
+            });
+
+            TweenLite.to(this.$options.slides[1], 0.575, {
+              alpha: 1,
+              delay: 2
+            });
           }
-        })
+        });
+      });
+    } catch (e) {
+      console.log(e);
     }
-  },
-  mounted: function () {
-    this.drawCanvas(this.mainImgID)
+
+    this.render();
   },
   methods: {
-    changeImg: function (imgID) {
-      this.drawCanvas(imgID)
-    },
-    drawCanvas: function(imgID) {
-      let cvs = document.getElementById('cvs')
-      cvs.width = 800
-      cvs.height = 540
-      let renderer = new PIXI.WebGLRenderer(cvs.width, cvs.height, {backgroundColor : 0xffffff, view: cvs, antialias: true} )
-      let container = new PIXI.Container()
-      let img = new Image()
-      img.src = "/images/detail/" + imgID + ".jpg";
-      img.onload = function(){
-        let sprite = new PIXI.Sprite(new PIXI.Texture(new PIXI.BaseTexture(img)))
-        sprite.width = cvs.width
-        sprite.height = cvs.height
-        container.addChild(sprite)
-        renderer.render(container)
-      }
-      let frag =
-        `precision mediump float;
-        uniform vec4 filterArea;
-        uniform sampler2D uSampler;
-        uniform float val;
-        varying vec2 vTextureCoord;
-        void main (void) {
-          vec4 col = texture2D(uSampler, vTextureCoord);
-          if(vTextureCoord.x > val) gl_FragColor = texture2D(uSampler, vec2(val, vTextureCoord.y));
-          else                      gl_FragColor = col;
-        }`.split('\n').reduce((c, a) => c + a.trim() + '\n')
-      let uniforms = {
-        val: { type: 'float', value: 0 }
-      };
-      let filter = new PIXI.Filter(null, frag, uniforms);
-      container.filters = [filter]
-      let count = 0;
-      function update(){
-        count += Math.pow(0.15 * Math.random(), 2);
-        uniforms = {
-          val: { type: 'float', value: count },
-          mappedMatrix : { type: 'mat3', value: new PIXI.Matrix() }
-        };
-        filter = new PIXI.Filter(null, frag, uniforms);
-        container.filters = [filter]
-        renderer.render(container)
-        requestAnimationFrame(update)
-      }
-      update();
+    render() {
+      this.$options.dispSprite.position.y += 4;
+      // this.$options.dispSprite.rotation += 0.33 / 10;
+
+      window.requestAnimationFrame(this.render.bind(this));
     }
   }
-}
+};
 </script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
