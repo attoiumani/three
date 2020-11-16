0622637fe9be3bdb813dcf4bea5d1fd6b5b11c0b
shirakawa <attoiumani0313@gmail.com>
2020-11-16 16:09:07 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <NLink to="/three" class="button--grey">three</NLink>
     <NLink to="/pixi" class="button--grey">pixi</NLink>
     <NLink to="/lottie" class="button--grey">lottie</NLink>
+    <NLink to="/gsap" class="button--grey">gsap</NLink>
   </div>
 </template>
 
