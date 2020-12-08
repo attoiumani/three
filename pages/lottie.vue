b1417ab0a3c305ab50b25fc6eadc9b33389449ed
shirakawa <attoiumani0313@gmail.com>
2020-12-08 12:28:17 +0900

    Update lottie.vue

--- pages/lottie.vue
+++ pages/lottie.vue
@@ -1,33 +1,43 @@
 <template>
-  <swiper
-    :slides-per-view="3"
-    :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    ...
-  </swiper>
+  <div>
+    <swiper :options="swiperOption">
+      <swiper-slide>スライダー１</swiper-slide>
+      <swiper-slide>スライダー２</swiper-slide>
+      <swiper-slide>スライダー３</swiper-slide>
+   
+    <div slot="pagination" class="swiper-pagination" />
+    <div slot="button-prev" class="swiper-button-prev" />
+    <div slot="button-next" class="swiper-button-next" />
+     </swiper>
+  </div>
 </template>
-<script>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
 
-  // Import Swiper styles
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    methods: {
-      onSwiper(swiper) {
-        console.log(swiper)
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        speed: 1000, //スライドの切り替わりスピード
+        spaceBetween: 30, //各スライドの余白
+        centeredSlides: true, //スライダーを真ん中に
+        loop: true, //無限ループ
+        autoplay: {
+          //スライドの自動切り替え
+          delay: 5000, //スライドの自動切り替えの秒数
+          disableOnInteraction: false, //何らかのアクション後の自動切り替えを再開
+        },
+        pagination: {
+          //ページネーション設定
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          //ナビゲーション設定
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
       },
-      onSlideChange() {
-        console.log('slide change')
-      },
-    },
-  };
+    };
+  },
+};
 </script>
